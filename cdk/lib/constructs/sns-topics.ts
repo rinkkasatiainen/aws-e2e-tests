3ada5750b41fddff9281288ac77a3bb75352e634
Aki Salmi <aki.salmi@iki.fi>
2020-04-10 11:17:55 +0300

    📝: update Step 2.3 and tips

--- cdk/lib/constructs/sns-topics.ts
+++ cdk/lib/constructs/sns-topics.ts
@@ -1,6 +1,5 @@
 import * as SNS from '@aws-cdk/aws-sns';
 import * as CDK from '@aws-cdk/core';
-import { env } from '../../bin/env';
 
 export type SnsTopicNames = 'SNS_TOPIC_ERRORS';
 
@@ -14,10 +13,6 @@ export type AllSnsTopics = {
 };
 
 // TODO Step 2.1: Define topic Name here - should be one defined in AllSnsTopics
-// tslint:disable-next-line:no-empty-interface
-export interface SnsTopics extends AllSnsTopics {
-    // SNS_TOPIC_ERRORS: SNS.ITopic;
-}
 
 // const createTopic: (stack: CDK.Stack, id: string) => SNS.ITopic =
 //     (stack, id) => {
@@ -29,7 +24,7 @@ export interface SnsTopics extends AllSnsTopics {
 //     };
 
 // TODO: Step 2.1. Use this to create all topics!
-export const createTopics: (stack: CDK.Stack) => SnsTopics =
+export const createTopics: (stack: CDK.Stack) => AllSnsTopics =
     () => {
         return {
         };
