487c60ade252b6d3335b5080abd2e5afb8290670
Aki Salmi <aki.salmi@iki.fi>
2020-04-26 17:33:29 +0300

    ✨: adding lambda to remove resources from dynamodb

--- cdk/lib/e2e-stack.ts
+++ cdk/lib/e2e-stack.ts
@@ -4,7 +4,7 @@ import {AllTables} from './constructs/dynamodb';
 import {AllSnsTopics} from './constructs/sns-topics';
 import {createLambda} from "./constructs/lambdas";
 import {failsMiserablyLambda} from "./constructs/lambdas/fails-miserably";
-// import {errorLogger} from "./constructs/lambdas/error-logger";
+import {errorLogger} from "./constructs/lambdas/error-logger";
 import {addCfnOutput} from "./constructs/cfn-output";
 
 export interface CreateStackProps {
@@ -18,22 +18,22 @@ export interface StackTopicProps {
 
 export const createStack: (stack: CDK.Stack, p: CreateStackProps) => void =
     (scope, {topics: {SNS_TOPIC_ERRORS}, tables}) => {
-        // TODO: Step 3.1 - Add fails miserably lambda
-        const {resourcesTable } = tables;
+        const {resourcesTable, errorsTable} = tables;
 
         const lambdaprod1 = createLambda
         (scope)
         ({envVars: {NODE_ENV: 'dev'}})
         (failsMiserablyLambda({resourcesTable})({SNS_TOPIC_ERRORS}));
 
-        // TODO: STEP 3.3:
+        createLambda
+        (scope)
+        ({envVars: {NODE_ENV: 'dev'}})
+        (errorLogger({errorsTable})({SNS_TOPIC_ERRORS}));
+
         addCfnOutput(scope)('lambdaThatFails')({
             value: lambdaprod1.functionName,
-            exportName: `${scope.stackName}:Lambda:lambdaThatFails`,
+            exportName: `${scope.stackName}:Lambda:prod1`,
         });
-        // TODO: Step 3.3: This is added for tests to warm up lambdas before the test run
-        // new CDK.CfnOutput(scope, `lambda-error-logger`, {
-        //     value: lambdaprod2.functionName,
-        //     exportName: `${scope.stackName}:Lambda:prod2`,
-        // });
+
+        return {}
     };
