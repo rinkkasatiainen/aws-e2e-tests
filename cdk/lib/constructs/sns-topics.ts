c8317ae54fab39b4a2bd9264ef65eadf693180ad
Aki Salmi <aki.salmi@iki.fi>
2020-04-26 12:33:24 +0300

    🎨: better types for topic handling"

--- cdk/lib/constructs/sns-topics.ts
+++ cdk/lib/constructs/sns-topics.ts
@@ -1,15 +1,14 @@
 import * as SNS from '@aws-cdk/aws-sns';
 import * as CDK from '@aws-cdk/core';
 
-export type SnsTopicNames = 'SNS_TOPIC_ERRORS';
-
 export type PossibleSnsTopics = {
-    [key in SnsTopicNames]?: SNS.ITopic;
+    SNS_TOPIC_ERRORS: SNS.ITopic;
 };
 
-// tslint:disable-next-line:no-empty-interface
+export type SnsTopicNames = keyof PossibleSnsTopics;
+
 export type AllSnsTopics = {
-    // [key in SnsTopicNames]: SNS.ITopic;
+    [key in SnsTopicNames]: SNS.ITopic;
 };
 
 // TODO Step 2.1: Define topic Name here - should be one defined in AllSnsTopics
